2a9be0cfc06f9f5349e76c253ad5fb20d9ad81bc
pthngws <pthngws@gmail.com>
2025-08-23 20:58:01 +0700

    pthngws register

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const redisClient = require("./config/redis");
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+connectDB();
+
+redisClient.on("connect", () => {
+  console.log("Redis connecting...");
+});
+redisClient.on("ready", () => {
+  console.log("✅ Redis connected & ready");
+});
+redisClient.on("error", (err) => {
+  console.error("❌ Redis error:", err);
 });
 
+
+const authRoutes = require("./routes/authRoute");
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
---
+++ utils/generateOtp.js
@@ -0,0 +1,4 @@
+function generateOtp(length = 6) {
+    return Math.floor(100000 + Math.random() * 900000).toString(); // 6 số
+}
+module.exports = generateOtp;
